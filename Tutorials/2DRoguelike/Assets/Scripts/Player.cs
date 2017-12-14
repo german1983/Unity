46e68ea8259fe0410ca608ed2e55e1391bb5a31b
German <german1983@gmail.com>
2017-12-13 22:42:04 -0400

    Initial commit

---
+++ Tutorials/2DRoguelike/Assets/Scripts/Player.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player : MovingObject {
+
+	public int wallDamage = 1;
+	public int pointsPerFood = 10;
+	public int pointsPerSoda = 20;
+	public float restartLevelDelay = 1f;
+	public Text foodText;
+
+	private Animator animator;
+	private int food;
+
+	// Use this for initialization
+	protected override void Start () {
+		animator = GetComponent<Animator> ();
+		food = GameManager.instance.playerFoodPoints;
+		foodText.text = "Food: " + food;
+		base.Start ();
+	}
+
+	private void OnDisable ()
+	{
+		//When Player object is disabled, store the current local food total in the GameManager so it can be re-loaded in next level.
+		GameManager.instance.playerFoodPoints = food;
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (!GameManager.instance.playerTurn)
+			return;
+
+		int horizontal = 0;
+		int vertical = 0;
+
+		horizontal = (int)Input.GetAxisRaw ("Horizontal");
+		vertical = (int)Input.GetAxisRaw ("Vertical");
+
+		if (horizontal != 0)
+			vertical = 0;
+
+		if (horizontal != 0 || vertical != 0)
+			AttemptMove<Wall> (horizontal, vertical);
+
+	}
+
+	protected override void AttemptMove<T>(int xDir, int yDir) {
+		food--;
+		foodText.text = "Food: " + food;
+
+		base.AttemptMove<T> (xDir, yDir);
+
+		RaycastHit2D hit;
+
+		CheckIfGameOver ();
+
+		GameManager.instance.playerTurn = false;
+	}
+
+	private void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Exit") {
+			Invoke ("Restart", restartLevelDelay);
+			enabled = false;
+		} else if (other.tag == "Food") {
+			food += pointsPerFood;
+			foodText.text = "+ " + pointsPerFood + " Food: " + food;
+			other.gameObject.SetActive (false);
+		} else if (other.tag == "Soda") {
+			food += pointsPerSoda;
+			foodText.text = "+ " + pointsPerSoda + " Food: " + food;
+			other.gameObject.SetActive (false);
+		}
+			
+	}
+
+	protected override void OnCantMove<T> (T component) {
+		Wall hitWall = component as Wall;
+		hitWall.DamageWall (wallDamage);
+		animator.SetTrigger ("playerChop");
+	}
+
+	private void Restart(){
+		// Application.LoadLevel (Application.loadedLevel);
+		// SceneManager.LoadScene(0);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
+	}
+
+	public void LoseFood(int loss) {
+		animator.SetTrigger ("playerHit");
+		food -= loss;
+		foodText.text = "- " + loss + " Food: " + food;
+		CheckIfGameOver ();
+	}
+
+	private void CheckIfGameOver(){
+		if (food <= 0)
+			GameManager.instance.GameOver ();
+	}
+}
---
+++ Tutorials/RandomMapGen/Assets/RandomMapGen/Scripts/Tile.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using UnityEngine;
+using System;
+using System.Text;
+
+public enum Sides {
+	Bottom,
+	Right,
+	Left,
+	Top
+}
+
+
+public class Tile  {
+	public int id = 0;
+	public Tile[] neighbors = new Tile[4];
+	public int autotileID;
+
+	public void addNeighbor(Sides side, Tile tile){
+		neighbors [(int)side] = tile;
+		CalculateAutotileID ();
+	}
+
+	public void removeNeighbor(Tile tile) {
+		var total = neighbors.Length;
+		for (var i = 0; i < total; i++) {
+			if (neighbors[i] != null) {
+				if (neighbors [i].id == tile.id) {
+					neighbors [i] = null;
+				}
+			}
+		}
+		CalculateAutotileID ();
+	}
+
+	public void clearNeighbors(){
+		var total = neighbors.Length;
+		for (var i = 0; i < total; i++) {
+			var tile = neighbors [i];
+			if (tile != null) {
+				tile.removeNeighbor (this);
+				neighbors [i] = null;
+			}
+		}
+		CalculateAutotileID ();
+	}
+
+	private void CalculateAutotileID(){
+		var sideValue = new StringBuilder ();
+		foreach (Tile tile in neighbors) {
+			sideValue.Append (tile == null ? "0" : "1");
+		}
+
+		autotileID = Convert.ToInt32 (sideValue.ToString (), 2);
+
+	}
+}
